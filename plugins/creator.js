cca5ccafe15c4fc282be8397fdcb047f00053c07
ramlaidi <83389072+ramlaidi@users.noreply.github.com>
2022-07-08 17:56:27 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
+*ɴᴀᴍᴇ:* irsan
+*ᴀɢᴇ:* 13
 *sᴛᴀᴛᴜs:* single 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* 5 April 2010
+*ᴀᴅᴅʀᴇss:* Indonesia, Jambi, KotaJambi
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
